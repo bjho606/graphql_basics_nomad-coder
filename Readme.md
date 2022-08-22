7b1e0008b6e7b1f906b734183cfa8d61794ebfb1
변재호 <bjho606@naver.com>
2022-08-22 14:41:19 +0900

    resolvers (query resolver, mutation resolver)

--- Readme.md
+++ Readme.md
@@ -16,28 +16,37 @@ Basics and concept of GraphQL
 
 <br>
 
----
-## GraphQL is a 'Schema-Definition-Language'(SDL)
-## => All GraphQL server should define <b>data types</b> (=schema)
+# GraphQL
+GraphQL is a 'Schema-Definition-Language'(SDL)<br>
 <br>
 
-## 1. Query Type (mandatory!)
+## 1. Types
+All GraphQL server should 'define' <b>data types</b> (=schema)
+### 1) Query Type (mandatory!)
     - 'type Query' is the most important and required type in GraphQL Server
     - It is the API that users are going to request
     - 'Query' in GraphQL is like 'GET Request' in Rest API (Get data)
 
-    1) Scalar Type
+    1. Scalar Type
         GraphQL provides basic scalar types such as String, Int, ID, ...
     
-    2) Object Type
+    2. Object Type
         combinations of scalar types can make a new user-made types
 
-## 2. Mutation Type
+### 2) Mutation Type
     - 'type Mutation' is used when user mutates the database (= server state)
     - 'Mutation' in GraphQL is like 'POST/DELETE/PUT/POST Request' in Rest API (modify data)
 
-## ※ Non Nullable Fields
+### ※ Non Nullable Fields
     All fields(=type) are 'nullable' by default
     If you want to make it 'not nullable', add '!' behind the type
     Choose if the fields or arguements are going to be nullable/non-nullable
+
+## 2. Resolvers
+Resolver is how the types(that are defined) should work
+=> Making a function for the types
+
+    'Resolver functions' must have the same name as 'Types',
+    'Arguements' (that is sent from user) are going to be in the 'SECOND' position(arguement) of the resolver function
+        -> the first arguement is 'the root arguement'
 
--- server.js
+++ server.js
@@ -4,6 +4,17 @@ import { ApolloServer, gql } from "apollo-server";
 // 2. normal way
 // const { ApolloServer, gql } = require("apollo-server");
 
+let tweets = [
+    {
+        id: "1",
+        text: "first hello",
+    },
+    {
+        id: "2",
+        text: "second hello",
+    },
+];
+
 // must explain the 'shape(type) of data'(=schema definition language) to graphql
 // 'typeDef' is making a gql structure
 // !![IMPORTANT] the 'type Query' is MANDATORY! (You must define it!)
@@ -18,12 +29,13 @@ const typeDefs = gql`
     type Tweet {
         id: ID!
         text: String!
-        author: User!
+        author: User
     }
 
     type Query {
         allTweets: [Tweet!]!
         tweet(id: ID!): Tweet
+        ping: String!
     }
 
     type Mutation {
@@ -32,7 +44,50 @@ const typeDefs = gql`
     }
 `;
 
-const server = new ApolloServer({typeDefs});
+// when a user requests a field in query type, apollo server will go to 'resolvers' and find the 'function' that has the same name with query type and call the function
+const resolvers = {
+    Query: {
+        // tweet(){
+        //     console.log("I'm called");
+        //     return null;
+        // },
+        // ping() {
+        //     return "pong";
+        // },
+
+        allTweets(){
+            return tweets;
+        },
+        
+        // user-input-arguements are 'ALWAYS' going to be in the second arguement in the resolver function
+        // tweet(root, args){
+        //     console.log(args);
+        //     return null;
+        // },
+        tweet(root, {id}){
+            return tweets.find(tweet => tweet.id === id);
+        }
+    },
+
+    Mutation: {
+        postTweet(_, { text, userId }){
+            const newTweet = {
+                id: tweets.length + 1,
+                text,
+            };
+            tweets.push(newTweet);
+            return newTweet;
+        },
+        deleteTweet(_, { id }){
+            const tweet = tweets.find(tweet => tweet.id === id);
+            if(!tweet) return false;
+            tweets = tweets.filter(tweet => tweet.id !== id);
+            return true;
+        },
+    },
+};
+
+const server = new ApolloServer({typeDefs, resolvers});
 
 server.listen().then(({ url }) => {
     console.log(`Running on ${url}`);
